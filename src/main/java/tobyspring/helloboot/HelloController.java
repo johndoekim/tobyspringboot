0a56942c38580dac4c0cc573204c61abedc44b26
junoh <amok0316@gmail.com>
2023-08-18 17:00:55 +0900

    230818

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,5 +1,10 @@
 package tobyspring.helloboot;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,19 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Objects;
 
 //클래스 레벨의 매핑을 찾은 후에 메쏘드 레벨을 찾음.
-@RequestMapping
+@RestController
 public class HelloController {
     //변수로 먼저 저장 하고 사용
     private final HelloService helloService;
+    private ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
+
+        System.out.println(applicationContext);
     }
 
-    //어노테이션을 넣으면 디스패쳐 서블릿이 빈을 다 뒤져서 웹 요청을 처리할 수 있는 매핑 정보를 처리 할 수 있는 클래스를 찾음
     @GetMapping("/hello")
-    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
+
 }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,43 +1,34 @@
 package tobyspring.helloboot;
 
-import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
+//컴포넌트란 애노테이션이 붙은 클래스를 등록해준다.
+//편리하기때문에 표준처럼 사용됨.
+//단점도 있지만 패키지 구성을 잘 하고 기술을 잘 하면 된다.
+@Configuration
+@ComponentScan
 public class HellobootApplication {
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory(){
+        return new TomcatServletWebServerFactory();
+    }
+    @Bean
+    public DispatcherServlet dispatcherServlet(){
+        return new DispatcherServlet();
+    }
 
     public static void main(String[] args) {
         //애플리케이션 컨텍스트는 즉 스프링 컨테이너
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
-        //메타정보를 넣어주는 방식으로 진행함
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
-        //구성 정보 초기화
-        applicationContext.refresh();
+        SpringApplication.run(HellobootApplication.class, args);
 
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet",
-                            new DispatcherServlet(applicationContext)
-                            //디스패쳐 서블릿은 제너릭웹애플리케이션컨텍스트를 이용
-                    ).addMapping("/*");
-        });
-        webServer.start();
     }
-
 }
